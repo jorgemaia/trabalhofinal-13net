af3a8f4dcf81a42ed626702d76fd4a3c582ef2c0
Guilherme Gentile <uk.gentile@gmail.com>
2019-03-10 15:26:39 -0300

    Trabalho Entregue - RMS - 330277-331760-331860-330284

---
+++ TrabalhoIOT/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading;
+using Microsoft.Azure.Devices.Client;
+
+namespace TrabalhoIOT
+{
+    class Program
+    {
+        private static readonly string _deviceConnectionString = "HostName=;DeviceId=;SharedAccessKey=";
+        private static TransportType _transportType = TransportType.Amqp;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite start para iniciar e stop para parar");
+
+            DeviceClient deviceClient = DeviceClient.CreateFromConnectionString(_deviceConnectionString, _transportType);
+
+            var monitor = new WeatherMonitor(deviceClient);
+
+            while (true)
+            {
+                var comando = Console.ReadLine();
+                if (comando == "stop")
+                {
+                    monitor.Stop();
+                }
+                if (comando == "start")
+                {
+                    ThreadPool.QueueUserWorkItem(BackgroundTask, monitor);
+                }
+            };
+        }
+
+        static void BackgroundTask(Object stateInfo)
+        {
+            WeatherMonitor monitor = (WeatherMonitor)stateInfo;
+            monitor.Start();
+        }
+    }
+}
